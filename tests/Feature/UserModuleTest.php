17da64cfd88990798598156da2db1b53efe545f2
Zolider <luis.vr25@gmail.com>
2018-04-23 10:20:14 -0500

    first commit

---
+++ tests/Feature/UserModuleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserModuleTest extends TestCase
+{
+    /** @test */
+    function it_loads_the_users_list_page()
+    {
+        $this->get('/usuarios')
+            ->assertStatus(200)
+            ->assertSee('Usuarios');
+    }
+
+    /** @test */
+    function it_loads_the_users_details_page()
+    {
+        $this->get('/usuarios/5')
+            ->assertStatus(200)
+            ->assertSee('Mostrando detalle del usuario: 5');
+    }
+
+    /** @test */
+    function it_loads_the_new_users_page()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->get('/usuarios/nuevo')
+            ->assertStatus(200)
+            ->assertSee('Crear nuevo usuario');
+    }
+
+    /** @test */
+    function it_loads_the_edit_users_name()
+    {
+
+        $this->get('/usuarios/5/edit')
+            ->assertStatus(200)
+            ->assertSee('Usuario 5 editado');
+    }
+}
